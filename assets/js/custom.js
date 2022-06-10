051ed53972fe79c25f32e11bc24498fc5f6503fe
Amanda Faro <amandafaro@hotmail.com>
2022-06-10 13:27:27 -0300

    alteração layout mobile

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -36,37 +36,37 @@
       }
   });
   
-  $("#modal_trigger").leanModal({
-		top: 100,
-		overlay: 0.6,
-		closeButton: ".modal_close"
-});
+//   $("#modal_trigger").leanModal({
+// 		top: 100,
+// 		overlay: 0.6,
+// 		closeButton: ".modal_close"
+// });
 
-$(function() {
-		// Calling Login Form
-		$("#login_form").click(function() {
-				$(".social_login").hide();
-				$(".user_login").show();
-				return false;
-		});
+// $(function() {
+// 		// Calling Login Form
+// 		$("#login_form").click(function() {
+// 				$(".social_login").hide();
+// 				$(".user_login").show();
+// 				return false;
+// 		});
 
-		// Calling Register Form
-		$("#register_form").click(function() {
-				$(".social_login").hide();
-				$(".user_register").show();
-				$(".header_title").text('Register');
-				return false;
-		});
+// 		// Calling Register Form
+// 		$("#register_form").click(function() {
+// 				$(".social_login").hide();
+// 				$(".user_register").show();
+// 				$(".header_title").text('Register');
+// 				return false;
+// 		});
 
-		// Going back to Social Forms
-		$(".back_btn").click(function() {
-				$(".user_login").hide();
-				$(".user_register").hide();
-				$(".social_login").show();
-				$(".header_title").text('Login');
-				return false;
-		});
-});
+// 		// Going back to Social Forms
+// 		$(".back_btn").click(function() {
+// 				$(".user_login").hide();
+// 				$(".user_register").hide();
+// 				$(".social_login").show();
+// 				$(".header_title").text('Login');
+// 				return false;
+// 		});
+// });
 
   // Acc
   $(document).on("click", ".naccs .menu div", function() {
@@ -115,30 +115,32 @@ $(function() {
     }
   });
 
-  $(document).ready(function () {
-      $(document).on("scroll", onScroll);
+  // removido por erro
+
+  // $(document).ready(function () {
+  //     $(document).on("scroll", onScroll);
       
-      //smoothscroll
-      $('.scroll-to-section a[href^="#"]').on('click', function (e) {
-          e.preventDefault();
-          $(document).off("scroll");
+  //     //smoothscroll
+  //     $('.scroll-to-section a[href^="#"]').on('click', function (e) {
+  //         e.preventDefault();
+  //         $(document).off("scroll");
           
-          $('.scroll-to-section a').each(function () {
-              $(this).removeClass('active');
-          })
-          $(this).addClass('active');
+  //         $('.scroll-to-section a').each(function () {
+  //             $(this).removeClass('active');
+  //         })
+  //         $(this).addClass('active');
         
-          var target = this.hash,
-          menu = target;
-          var target = $(this.hash);
-          $('html, body').stop().animate({
-              scrollTop: (target.offset().top) + 1
-          }, 500, 'swing', function () {
-              window.location.hash = target;
-              $(document).on("scroll", onScroll);
-          });
-      });
-  });
+  //         var target = this.hash,
+  //         menu = target;
+  //         var target = $(this.hash);
+  //         $('html, body').stop().animate({
+  //             scrollTop: (target.offset().top) + 1
+  //         }, 500, 'swing', function () {
+  //             window.location.hash = target;
+  //             $(document).on("scroll", onScroll);
+  //         });
+  //     });
+  // });
 
   function onScroll(event){
       var scrollPos = $(document).scrollTop();
