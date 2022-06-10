9ae0506cff07353304635dcec164267fec9d3abf
Amanda Faro <amandafaro@hotmail.com>
2022-06-10 14:43:24 -0300

    atualização corpo do e-mail

--- assets/email/enviar_email.php
+++ assets/email/enviar_email.php
@@ -27,31 +27,50 @@
     use PHPMailer\PHPMailer\Exception;
 
     $mail = new PHPMailer(true);
-    $mail->isSMTP(); // Não modifique
-    $mail->Host       = 'servidor.hostgator.com.br';  // SEU HOST (HOSPEDAGEM)
-    $mail->SMTPAuth   = true;                        // Manter em true
-    $mail->Username   = 'contato@syncher.com.br';   //SEU USUÁRIO DE EMAIL
-    $mail->Password   = '@Turner505';                   //SUA SENHA DO EMAIL SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;    //TLS OU SSL-VERIFICAR COM A HOSPEDAGEM
-    $mail->Port       = 465;     //TCP PORT, VERIFICAR COM A HOSPEDAGEM
-    $mail->CharSet = 'UTF-8';    //DEFINE O CHARSET UTILIZADO
-    
-    //Recipients
-    $mail->setFrom('contato@syncher.com.br', 'Site');  //DEVE SER O MESMO EMAIL DO USERNAME
-    $mail->addAddress('amandafaro@hotmail.com');     // QUAL EMAIL RECEBERÁ A MENSAGEM!
-    // $mail->addAddress('ellen@example.com');    // VOCÊ pode incluir quantos receptores quiser
-    $mail->addReplyTo('amandafaro@hotmail.com', $nome);  //AQUI SERA O EMAIL PARA O QUAL SERA RESPONDIDO                  
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = utf8_decode('Mensagem do Formulário'); //ASSUNTO
-    $mail->Body    = utf8_decode($mensagem);  //CORPO DA MENSAGEM
-    $mail->AltBody = utf8_decode($mensagem);  //CORPO DA MENSAGEM EM FORMA ALT
 
-    // $mail->send();
-    if(!$mail->Send()) {
-        echo "<script>alert('Erro ao enviar o E-Mail');window.location.assign('index.php');</script>";
-     }else{
-        echo "<script>alert('E-Mail enviado com sucesso!');window.location.assign('index.php');</script>";
-     }
-     die
+    try{
+        $email -> SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->Host       = 'servidor.hostgator.com.br';  // SEU HOST (HOSPEDAGEM)
+        $mail->SMTPAuth   = true;                        // Manter em true
+        $mail->Username   = 'contato@syncher.com.br';   //SEU USUÁRIO DE EMAIL
+        $mail->Password   = '@Turner505';                   //SUA SENHA DO EMAIL SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;    //TLS OU SSL-VERIFICAR COM A HOSPEDAGEM
+        $mail->Port       = 465;     //TCP PORT, VERIFICAR COM A HOSPEDAGEM
+        
+        $mail->setFrom('contato@syncher.com.br', 'Site');  //DEVE SER O MESMO EMAIL DO USERNAME
+        $mail->addReplyTo('amandafaro@hotmail.com', $nome);  //AQUI SERA O EMAIL PARA O QUAL SERA RESPONDIDO                  
+        
+        $mail->addAddress('amandafaro@hotmail.com');     // QUAL EMAIL RECEBERÁ A MENSAGEM!
+
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = utf8_decode('Mensagem do Formulário'); //ASSUNTO
+        $mail->Body    = utf8_decode($mensagem);  //CORPO DA MENSAGEM
+        $mail->AltBody = utf8_decode($mensagem);  //CORPO DA MENSAGEM EM FORMA ALT
+
+        $mail->send();
+        
+        echo 'e-mail enviado'
+
+    }catch(Exception $e){
+        echo 'erro ' . $e->getMessage()
+
+    }
+    
+    // $mail->isSMTP(); // Não modifique
+    // $mail->CharSet = 'UTF-8';    //DEFINE O CHARSET UTILIZADO
+    
+    // //Recipients
+    // // Content
+    // $mail->isHTML(true);                                  // Set email format to HTML
+    // $mail->Subject = utf8_decode('Mensagem do Formulário'); //ASSUNTO
+    // $mail->Body    = utf8_decode($mensagem);  //CORPO DA MENSAGEM
+    // $mail->AltBody = utf8_decode($mensagem);  //CORPO DA MENSAGEM EM FORMA ALT
+
+    // // $mail->send();
+    // if(!$mail->Send()) {
+    //     echo "<script>alert('Erro ao enviar o E-Mail');window.location.assign('index.php');</script>";
+    //  }else{
+    //     echo "<script>alert('E-Mail enviado com sucesso!');window.location.assign('index.php');</script>";
+    //  }
+    //  die
 ?>
